04e9c488de71446b9e757b2e8123780939fceb95
sakthimugupython <mugunthanpythononline@vetritechnologysolutions.in>
2025-10-13 10:48:32 +0530

    Fix download without save - use blob response instead of JSON

--- Payslip/settings.py
+++ Payslip/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6t++33*em2qr$a$4ciq@&i4(46u#0r^ry5o#b2u@nj*=vsz3ts'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
